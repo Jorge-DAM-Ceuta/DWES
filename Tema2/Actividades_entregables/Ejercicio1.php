d445318f30c7b00759397ffcd61418fd6bdd0fd0
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-09-28 13:08:07 +0200

    E1

---
+++ Tema2/Actividades_entregables/Ejercicio1.php
@@ -0,0 +1,91 @@
+<?php 
+
+$primeraPalabraIngles = "Turtle";
+$primeraPalabraEspanol = "Tortuga";
+
+$segundaPalabraIngles = "Swimming pool";
+$segundaPalabraEspanol = "Piscina";
+
+$terceraPalabraIngles = "Pencil";
+$terceraPalabraEspanol = "Lápiz";
+
+$cuartaPalabraIngles = "Language";
+$cuartaPalabraEspanol = "Lenguaje";
+
+$quintaPalabraIngles = "Edit";
+$quintaPalabraEspanol = "Editar";
+
+$sextaPalabraIngles = "Picture";
+$sextaPalabraEspanol = "Dibujo";
+
+$septimaPalabraIngles = "Videogame";
+$septimaPalabraEspanol = "Videojuego";
+
+$octavaPalabraIngles = "Train";
+$octavaPalabraEspanol = "Entrenar";
+
+$novenaPalabraIngles = "Run";
+$novenaPalabraEspanol = "Correr";
+
+$decimaPalabraIngles = "Try";
+$decimaPalabraEspanol = "Probar";
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Inglés</th>
+            <th>Español</th>
+        </tr>
+        
+        <tr>
+            <td><?php echo $primeraPalabraIngles?></td>
+            <td><?php echo $primeraPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $segundaPalabraIngles?></td>
+            <td><?php echo $segundaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $terceraPalabraIngles?></td>
+            <td><?php echo $terceraPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $cuartaPalabraIngles?></td>
+            <td><?php echo $cuartaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $quintaPalabraIngles?></td>
+            <td><?php echo $quintaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $sextaPalabraIngles?></td>
+            <td><?php echo $sextaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $septimaPalabraIngles?></td>
+            <td><?php echo $septimaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $octavaPalabraIngles?></td>
+            <td><?php echo $octavaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $novenaPalabraIngles?></td>
+            <td><?php echo $novenaPalabraEspanol?></td>
+        </tr>
+        <tr>
+            <td><?php echo $decimaPalabraIngles?></td>
+            <td><?php echo $decimaPalabraEspanol?></td>
+        </tr>
+
+    </table>
+</body>
+</html>
