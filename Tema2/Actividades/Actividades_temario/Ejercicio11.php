fe47ffddaeef4f10d696bb23477a1691b00cc590
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-10-05 13:09:09 +0200

    pasoPorReferencia

--- Tema2/Actividades/Actividades_temario/Ejercicio11.php
+++ Tema2/Actividades/Actividades_temario/Ejercicio11.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-</html>
+<?php
+/*
+$nombre = "CARLITOS";
+
+function cambiarNombre(&$nombreNuevo){
+    $nombreNuevo = "JORGE";
+}
+
+cambiarNombre($nombre);
+
+echo $nombre;
+*/
+?>
