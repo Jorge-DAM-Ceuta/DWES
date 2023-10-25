d14d9d48a63692ea717d468a1a9a2bdf87b0f23e
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-10-25 22:10:34 +0200

    Ejercicios N3

--- Tema3/Actividades/EjerciciosProgramacionN3/Ejercicio1.php
+++ Tema3/Actividades/EjerciciosProgramacionN3/Ejercicio1.php
@@ -8,14 +8,30 @@
     <body>
         <?php
 
+            /*En este ejercicio vamos a almacenar las cartas obtenidas en un array que se obtendrán a partir
+            del palo de la carta, su valor y la puntuación de cada una. También almacenaremos la puntuación 
+            de cada una en una variable resultado. */
+
             $cartas = array("As", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Sota", "Caballo", "Rey");
             $palo = array("Oro", "Basto", "Espada", "Copa");
             $puntos = array("As" => 11, "Dos" => 0, "Tres" => 10, "Cuatro" => 0, "Cinco" => 0, "Seis" => 0, "Siete" => 0, "Sota" => 2, "Caballo" => 3, "Rey" => 4);
             
             $cartasObtenidas = array();
-
             $resultado = 0;
             
+            /*En este bucle for se escogen 10 cartas aleatoriamente mediante un palo y un número.
+
+            A partir de estos valores se obtiene la puntuación de la carta y se almacena la 
+            información de la carta en una variable para mostrar la carta obtenida en cada vuelta.
+            
+            Si la carta que hemos obtenido no se encuentra en la baraja se cuenta, en caso contrario
+            se disminuye el valor del iterador $i para obtener otra distinta. En caso de que no se 
+            encuentre en el array de las cartas obtenidas se añade dicha carta con array_push() y se
+            suma al resultado los puntos correspondientes obtenidos de la carta, además se muestra la 
+            carta obtenida por pantalla con el número de la tirada.
+
+            Por último se muestra la puntuación total obtenida de todas las cartas.
+            */
             for($i = 0; $i<10; $i++){
                 $paloActual = $palo[random_int(0, 3)];
                 $numeroActual =  $cartas[random_int(0,9)];
@@ -28,7 +44,7 @@
                     array_push($cartasObtenidas, $cartaObtenida);
                     $resultado += $puntosActual;
                     
-                    echo "<p>" . $cartaObtenida . "</p>";
+                    echo "<p>Carta número: " . $i . ": " . $cartaObtenida . "</p>";
 
                 }else{
                     $i--;
---
+++ Tema3/Actividades/EjerciciosProgramacionN3/Ejercicio4.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicio 4</title>
+    </head>
+    <body>
+        
+        <form name="input" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            <p>
+                <label name="inputFilas">Número de filas:</label>
+                <input name="filas" id="filas" type="number">
+            </p>
+            
+            <p>
+                <label name="inputColumnas">Número de columnas:</label>
+                <input name="columnas" id="columnas" type="number">
+            </p>
+            
+            <p>
+                <input name="enviar" value="Generar tabla" type="submit">
+            </p>
+        </form>
+
+        <?php
+            if (isset($_POST['enviar'])) {
+                $filas = $_POST["filas"];
+                $columnas = $_POST["columnas"];
+        
+                if ($filas > 0 && $columnas > 0) {
+                    echo "<table border='1'>";
+
+                    for ($i = 1; $i <= $filas; $i++) {
+                        echo '<tr>';
+                        
+                        for ($j = 1; $j <= $columnas; $j++) {
+                            $numero = $i + $j;
+                            $estilo = "color: black;";
+
+                            if ($numero % 3 == 0){
+                                $estilo = "color: red;";
+                            } 
+
+                            echo "<td style='". $estilo . "'>" . $numero . "</td>";
+                        }
+
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                
+                }
+            }
+        ?>
+    </body>
+</html>
