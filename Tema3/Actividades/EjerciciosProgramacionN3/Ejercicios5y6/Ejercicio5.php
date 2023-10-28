6c882e2025c3ee548dbb4016905938d30b258d28
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-10-28 18:11:29 +0200

    Ejercicio5 ?

--- Tema3/Actividades/EjerciciosProgramacionN3/Ejercicios5y6/Ejercicio5.php
+++ Tema3/Actividades/EjerciciosProgramacionN3/Ejercicios5y6/Ejercicio5.php
@@ -28,11 +28,11 @@
                 que el elemento del array contenga una extensión permitida usando pathinfo() de la posición 
                 correspondiente del array y como segundo parámetro PATHINFO_EXTENSION que devolverá la extensión 
                 de cada elemento. Si contiene alguna de las extensiones se pinta una fila HTML y en otro bucle for
-                anidado se crean 4 filas en su interior iterando con el valor de $i, la condición de parada es un 
-                contador que empieza en 0 en cada vuelta del primer for y que debe llegar hasta 3 para pintar 4 
-                columnas. Al terminar de pintar las 4 filas se suma a $i el valor de $contador para que continúe 
-                iterando desde la última foto mostrada. Por último se cierra la etiqueta de la fila y se sigue en
-                el primer bucle. */
+                anidado se crean 4 columnas en su interior iterando a partir del valor de $i, la condición de parada 
+                es un contador que empieza en 0 en cada vuelta del primer for y que debe llegar hasta 3 para pintar 4 
+                imagenes en la fila. Al terminar de pintar las 4 filas se suma a $i el valor de $contador para que 
+                continúe iterando desde la última foto mostrada. Por último se cierra la etiqueta de la fila y se sigue 
+                en el primer bucle. */
                 echo "<table border='1'>";
 
                 for($i = 0; $i < count($imagenes); $i++){
@@ -44,8 +44,14 @@
                         echo "<tr>";
 
                         for($j = $i; $contador < 4; $j++){
-                            echo "<td><img src='./imagenes/" . $imagenes[$j] . "' width='100%' height='100%'></td>";
-                            $contador++;
+                            if($j == count($imagenes)){
+                                echo "<td><img src='./imagenes/" . $imagenes[$j] . "' width='100%' height='100%'></td>";
+                                break;    
+                            }else{
+                                echo "<td><img src='./imagenes/" . $imagenes[$j] . "' width='100%' height='100%'></td>";
+                                $contador++;
+                            }
+                            
                         }
 
                         $i += $contador;
