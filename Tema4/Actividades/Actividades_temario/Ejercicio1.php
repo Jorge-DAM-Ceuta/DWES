dd1439b7095b84a53e74374a5328ae3d442ec5ca
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-01-08 20:53:25 +0100

    Ejercicios investigación

---
+++ Tema4/Actividades/Actividades_temario/Ejercicio1.php
@@ -0,0 +1,15 @@
+<?php
+
+    /*
+        1. Tienda online: Se usan variables comunes como el carrito de la compra,
+        la información del usuario y de los productos. Los datos se comparten 
+        entre la página de productos, la página de pagos, etc...
+        
+        2. Wordpress o Joomla: Tienen variables comunes para incluir información
+        del sitio web, la sesión del usuario y los datos de los contenidos.
+
+        3. Redes sociales: Las variables comunes en una red social pueden ser el
+        nombre de usuario, información de la sesión y las notificaciones.
+    */
+
+?>
---
+++ Tema4/Actividades/Actividades_temario/Ejercicio2.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+        Aplicaciones web que usan:
+        
+            1. Cookies: En una tienda online, para cambiar el idioma, establecer las 
+            políticas de proivacidad o guardar los productos del carrito.
+
+            2. Sesiones: En una aplicación web con login, se usan para la autenticación 
+            de los usuarios así como su inicio de sesión donde se guardan su identidad 
+            y permisos permitiéndo ver ciertos contenidos o realizar determinadas acciones.
+
+            3. Sesiones y cookies: En una red social se usan cookies para las preferencias 
+            como el idioma y las sesiones para mantener la sesión iniciada del usuario.
+    */
+
+?>
