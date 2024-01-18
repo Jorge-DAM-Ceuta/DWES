1c851ae3c5f06577ee0c28211899739d606230ba
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-01-18 13:16:49 +0100

    Comienzo del proyecto

---
+++ Tema5/Reproductor_de_musica/src/clases/Cancion.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Cancion{
+        private string $id;
+        private string $titulo;
+        private string $artista;
+        private array $colaboracion;
+        private int $duracion;
+        private bool $favorita;
+
+        public function __construct($artista){
+
+        }
+    }
+
+?> 
