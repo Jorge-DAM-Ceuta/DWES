7a11a0bdd5b0aa5e51557dc0c1a2489711eead67
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-27 10:45:40 +0100

    Proyecto composer

---
+++ Tema8/Actividades/Ej1_Tema7_Composer/index.php
@@ -0,0 +1,7 @@
+<?php
+    require("vendor/autoload.php");
+
+    include_once("src/Views/Assets/Templates/AperturaIndex.php");
+    include_once("src/Views/Articulos.php");
+    include_once("src/Views/Assets/Templates/Cierre.php");
+?>
---
+++ Tema8/Actividades/Ej1_Tema7_Composer/src/Views/Assets/Templates/AperturaIndex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang='es'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='src/Views/Assets/CSS/Index.css'>
+        <title>Blog</title>
+    </head>
+    <body>
+    <header>
+        <h1>Artículos de interés</h1>
+        <a class='publicar' href='src/Views/InsertarForm.php'>Publicar artículo</a>            
+    </header>
